51435bf9a58c86129f3fb66c184699b9e05e9899
Halim <halimputra0701@gmail.com>
2024-11-01 22:22:54 +0700

    refactor(config): update db configuration

--- src/configs/knexConfig.ts
+++ src/configs/knexConfig.ts
@@ -1,5 +1,18 @@
+import dotenv from 'dotenv';
 import knex from 'knex';
-import config from '../../knexfile';
 
-const db = knex(config);
+// Pastikan dotenv dijalankan di sini
+dotenv.config({ path: '.env.local' });
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		port: Number(process.env.DB_PORT),
+	},
+});
+
 export default db;
