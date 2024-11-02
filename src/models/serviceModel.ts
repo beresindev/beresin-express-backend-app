9b06a60f055f6ea2cd7f932173f0b890886dbd88
Halim <halimputra0701@gmail.com>
2024-11-03 03:04:21 +0700

    feat(model): add serviceModel

---
+++ src/models/serviceModel.ts
@@ -0,0 +1,48 @@
+// models/serviceModel.ts
+import db from '../configs/knexConfig';
+
+interface Service {
+	id: number;
+	user_id: number;
+	isSubscription: boolean;
+	name_of_service: string;
+	category_id: number;
+	description: string;
+	status: 'pending' | 'decline' | 'accept';
+	created_at?: Date;
+	updated_at?: Date;
+}
+
+const serviceModel = {
+	create: async (serviceData: Partial<Service>): Promise<Service> => {
+		const [newService] = await db<Service>('service').insert(serviceData).returning('*');
+		return newService;
+	},
+	findAll: async (): Promise<Service[]> => {
+		return db<Service>('service').select('*');
+	},
+	// models/serviceModel.ts
+	findById: async (id: number): Promise<Service | undefined> => {
+		return db<Service>('service').where({ id }).first();
+	},
+
+	findByUserId: async (userId: number): Promise<Service[]> => {
+		return db<Service>('service').where({ user_id: userId }).select('*');
+	},
+	updateById: async (id: number, updates: Partial<Service>): Promise<Service | undefined> => {
+		const [updatedService] = await db<Service>('service').where({ id }).update(updates).returning('*');
+		return updatedService;
+	},
+	deleteById: async (id: number): Promise<number> => {
+		return db<Service>('service').where({ id }).del();
+	},
+	updateStatus: async (id: number, status: 'accept' | 'decline' | 'pending'): Promise<Service | undefined> => {
+		const [updatedService] = await db<Service>('service').where({ id }).update({ status }).returning('*');
+		return updatedService;
+	},
+	findAllApproved: async (): Promise<Service[]> => {
+		return db<Service>('service').where({ status: 'accept' }).select('*');
+	},
+};
+
+export default serviceModel;
