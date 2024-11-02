d43687d9209d88ff368f4e87fb42a505bb092ee6
Halim <halimputra0701@gmail.com>
2024-11-03 02:55:05 +0700

    refactor(core): add auth and user routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,42 @@
 import express from 'express';
 import { errorHandler } from './handlers/errorHandler';
-import authRoutes from './routes/v1/authRoute';
-import userRoutes from './routes/v1/userRoute';
+import authRoute from './routes/v1/authRoute';
+import userRoute from './routes/v1/userRoute';
+
+// Routes untuk User
+import userServiceRoute from './routes/v1/user/userServiceRoute';
+import userCategoryRoute from './routes/v1/user/userCategoryRoute';
+
+// Routes untuk Admin
+import adminCategoryRoute from './routes/v1/admin/adminCategoryRoute';
+import adminServiceRoute from './routes/v1/admin/adminServiceRoute';
+
+// Routes umum
+import categoryRoute from './routes/v1/categoryRoute';
+import imageRoute from './routes/v1/user/userImageRoute';
+import serviceRoute from './routes/v1/serviceRoute';
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/user', userRoutes);
+// Route untuk autentikasi
+app.use('/api/v1/auth', authRoute);
 
-// Tambahkan handler untuk root URL
-app.get('/', (req, res) => {
-	res.send('Welcome to the API');
-});
+// Route untuk user
+app.use('/api/v1/user', userRoute);                 // Mengelola profil user
+app.use('/api/v1/user/services', userServiceRoute); // CRUD layanan milik user
+app.use('/api/v1/user/category', userCategoryRoute); // Mengelola kategori layanan untuk user
+app.use('/api/v1/user/image', imageRoute);                // Mengelola upload gambar
 
-// Error handling middleware
+// Route umum yang bisa diakses semua pengguna
+app.use('/api/v1/services', serviceRoute); // Melihat semua layanan yang disetujui
+app.use('/api/v1/category', categoryRoute);          // Menampilkan semua kategori
+
+// Route khusus admin
+app.use('/api/v1/admin/category', adminCategoryRoute); // CRUD kategori layanan oleh admin
+app.use('/api/v1/admin/services', adminServiceRoute);   // Manajemen layanan oleh admin
+
+// Middleware penanganan error
 app.use(errorHandler);
 
 export default app;
