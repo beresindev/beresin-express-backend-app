22486c0db7157414135acebd67ec301fd90e357c
Halim <halimputra0701@gmail.com>
2024-11-16 01:44:39 +0700

    refactor(route): update route into choose file and add max 5MB

--- src/routes/v1/user/userServiceRoute.ts
+++ src/routes/v1/user/userServiceRoute.ts
@@ -6,18 +6,20 @@ import { allowRoles } from '../../../middlewares/roleMIddleware';
 
 const router = express.Router();
 
-// Configure multer only for multipart/form-data requests
-const storage = multer.memoryStorage(); // Using memory storage to handle both types
-const upload = multer({ storage });
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file limit
+});
 
 const detectMultipart = (req: Request, res: Response, next: NextFunction) => {
-	if (req.is('multipart/form-data')) {
-		return upload.array('images', 2)(req, res, next); // Use multer for multipart requests
-	}
-	next(); // Continue without multer if it's not multipart
+    if (req.is('multipart/form-data')) {
+        return upload.array('images', 10)(req, res, next);
+    }
+    next();
 };
 
-// Routes setup
+// Routes
 router.post('/', authenticateToken, allowRoles(['User']), detectMultipart, createServiceWithImages);
 router.get('/', authenticateToken, allowRoles(['User']), getUserServices);
 router.put('/:id', authenticateToken, allowRoles(['User']), detectMultipart, updateUserService);
