9abcef0cd5efac666adaed6e7351e09338f7f4c9
Halim <halimputra0701@gmail.com>
2024-11-18 12:58:59 +0700

    refactor(route): add user route getUserServiceById

--- src/routes/v1/user/userServiceRoute.ts
+++ src/routes/v1/user/userServiceRoute.ts
@@ -1,6 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
 import multer from 'multer';
-import { createServiceWithImages, deleteUserService, getUserServices, updateUserService } from '../../../controllers/user/serviceController';
+import {
+	createServiceWithImages,
+	deleteUserService,
+	getServiceById,
+	getUserServices, 
+	updateUserService,
+} from '../../../controllers/user/serviceController';
 import { authenticateToken } from '../../../middlewares/authMiddleware';
 import { allowRoles } from '../../../middlewares/roleMIddleware';
 
@@ -8,20 +14,21 @@ const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file limit
+	storage,
+	limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file limit
 });
 
 const detectMultipart = (req: Request, res: Response, next: NextFunction) => {
-    if (req.is('multipart/form-data')) {
-        return upload.array('images', 10)(req, res, next);
-    }
-    next();
+	if (req.is('multipart/form-data')) {
+		return upload.array('images', 10)(req, res, next);
+	}
+	next();
 };
 
 // Routes
-router.post('/', authenticateToken, allowRoles(['User']), detectMultipart, createServiceWithImages);
 router.get('/', authenticateToken, allowRoles(['User']), getUserServices);
+router.get('/:id', authenticateToken, allowRoles(['User']), getServiceById);
+router.post('/', authenticateToken, allowRoles(['User']), detectMultipart, createServiceWithImages);
 router.put('/:id', authenticateToken, allowRoles(['User']), detectMultipart, updateUserService);
 router.delete('/:id', authenticateToken, allowRoles(['User']), deleteUserService);
 
