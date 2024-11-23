4ce69bfc9a8ce03592da066c2e4e41ad1779b062
Halim <halimputra0701@gmail.com>
2024-11-23 22:55:41 +0700

    refactor(migration): update isSubscription, add into notNullable

--- src/migrations/20241101145223_create_service_table.ts
+++ src/migrations/20241101145223_create_service_table.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 		table.timestamp('updated_at').defaultTo(knex.fn.now());
 		table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-		table.boolean('isSubscription').defaultTo(false);
+		table.boolean('isSubscription').defaultTo(false).notNullable(); 
 		table.string('name_of_service').notNullable();
 		table.integer('category_id').unsigned().notNullable().references('id').inTable('category_services').onDelete('SET NULL');
 		table.text('description').notNullable();
