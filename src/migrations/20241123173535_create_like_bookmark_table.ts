eea5437180b099b03eb589fa0499a9ae96cd96dc
Halim <halimputra0701@gmail.com>
2024-11-24 02:42:40 +0700

    refactor(migration): remove comment

--- src/migrations/20241123173535_create_like_bookmark_table.ts
+++ src/migrations/20241123173535_create_like_bookmark_table.ts
@@ -2,14 +2,14 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('service', (table) => {
-		table.integer('like_count').unsigned().notNullable().defaultTo(0); // Tambahkan kolom like_count
-		table.integer('bookmark_count').unsigned().notNullable().defaultTo(0); // Tambahkan kolom bookmark_count
+		table.integer('like_count').unsigned().notNullable().defaultTo(0); 
+		table.integer('bookmark_count').unsigned().notNullable().defaultTo(0);
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('service', (table) => {
-		table.dropColumn('like_count'); // Hapus kolom like_count jika di-rollback
-		table.dropColumn('bookmark_count'); // Hapus kolom bookmark_count jika di-rollback
+		table.dropColumn('like_count'); 
+		table.dropColumn('bookmark_count');
 	});
 }
