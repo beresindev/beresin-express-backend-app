f16580618019d277bd69082dc519f24a90159d7f
Halim <halimputra0701@gmail.com>
2024-12-01 17:52:23 +0700

    refactor(root): update dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import app from './app';
 
-dotenv.config({ path: '.env.local' });
+dotenv.config;
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
